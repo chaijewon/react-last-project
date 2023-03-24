2bf29b9ae3cd2c593e09d73bfe5286d99e250bed
chaijwon <vcandjava@nate.com>
2023-03-24 10:42:40 +0900

    commit-jeju2

--- src/components/food/FoodFind.js
+++ src/components/food/FoodFind.js
@@ -22,12 +22,70 @@ function FoodFind(){
     },[])
     const dataChange=(e)=>{
         setSs(e.target.value)
-    }
 
+    }
+    const dataKeyDown=(e)=>{
+        setCurpage(1)
+        if(e.keyCode==13)
+        {
+            axios.get("http://localhost/jeju/food_find_react",{
+                params:{
+                    page:curpage,
+                    title:ss
+                }
+            }).then(response=>{
+                console.log(response.data)
+                setFdata(response.data)
+                setCurpage(response.data[0].curpage)
+                setTotalpage(response.data[0].totalpage)
+            })
+        }
+    }
     const findData=()=>{
        //alert(ss)
+        setCurpage(1)
+        axios.get("http://localhost/jeju/food_find_react",{
+            params:{
+                page:curpage,
+                title:ss
+            }
+        }).then(response=>{
+            console.log(response.data)
+            setFdata(response.data)
+            setCurpage(response.data[0].curpage)
+            setTotalpage(response.data[0].totalpage)
+        })
     }
+    const prev=()=>{
+        const page=curpage>1?curpage-1:curpage;
 
+        axios.get("http://localhost/jeju/food_find_react",{
+            params:{
+                page:page,
+                title:ss
+            }
+        }).then(response=>{
+            console.log(response.data)
+            setFdata(response.data)
+
+        })
+        setCurpage(page)
+    }
+    const next=()=>{
+        const page=curpage<totalpage?curpage+1:curpage
+
+        axios.get("http://localhost/jeju/food_find_react",{
+            params:{
+                page:page,
+                title:ss
+            }
+        }).then(response=>{
+            console.log(response.data)
+            setFdata(response.data)
+
+        })
+        setCurpage(page)
+    }
     let html=fdata.map((food,index)=>
         <li className={index%4==0?'one_quarter first':'one_quarter'}>
             <NavLink to={"/jeju/food_detail/"+food.no}>
@@ -44,7 +102,8 @@ function FoodFind(){
                     <div id="gallery">
                         <figure>
                             <header className="heading inline">
-                                <input type={"text"} size={"30"} className={"input-sm"} onChange={dataChange} value={ss}/>
+                                <input type={"text"} size={"30"} className={"input-sm"}
+                                       onChange={dataChange} value={ss} onKeyDown={dataKeyDown}/>
                                 <input type={"button"} value={"검색"}
                                        className={"btn btn-sm btn-danger"} onClick={findData}/>
                             </header>
@@ -56,6 +115,11 @@ function FoodFind(){
                     </div>
 
 
+                </div>
+                <div className={"text-center"}>
+                    <button className={"btn btn-sm btn-primary"} onClick={prev}>이전</button>
+                    {curpage} page / {totalpage} pages
+                    <button className={"btn btn-sm btn-primary"} onClick={next}>다음</button>
                 </div>
 
                 <div className="clear"></div>
